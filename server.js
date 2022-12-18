feb0147dda4715bead27d3f9aaf229d7d2f4a9eb
Pawan Thakuni <thakunipawan@gmail.com>
2022-12-18 16:37:08 +0530

    second

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-console.log('hey therer')
+const express=require('express')
+const env=require('dotenv')
+env.config({path:'./env'})
+const app=express()
+const PORT=process.env.PORT || 5000
+app.listen(PORT,console.log(`server running in port ${process.env.NODE_ENV}on port ${PORT} `))
