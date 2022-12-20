cf8e3bf3efb53b503c6f8d104fb4c477bc5bc91e
Pawan Thakuni <thakunipawan@gmail.com>
2022-12-20 14:54:04 +0530

    third

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express=require('express')
 const env=require('dotenv')
 env.config({path:'./env'})
+
 const app=express()
+app.get('/',(req,res)=>{
+    res.send('hello world  nice to see you hello world again;');
+})
 const PORT=process.env.PORT || 5000
 app.listen(PORT,console.log(`server running in port ${process.env.NODE_ENV}on port ${PORT} `))
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+
+const PORT=process.env.PORT || 5000
+app.listen(PORT,console.log(`server running in port ${process.env.NODE_ENV}on port ${PORT} `))
